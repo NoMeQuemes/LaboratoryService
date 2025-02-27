5cbfdc46696e229f4fa128f4b201c40284484ba0
NoMeQuemes <munozfernando264@gmail.com>
2025-02-26 23:27:36 -0300

    Se agregaron segmentos faltantes del mensaje HL7

--- LaboratoryService_Api/Program.cs
+++ LaboratoryService_Api/Program.cs
@@ -1,10 +1,13 @@
 using LaboratoryService_Api.Data;
+using LaboratoryService_Api.Utilities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<ConvertHL7>(); // Acá se agregan el archivo para convertir a HL7
+builder.Services.AddSingleton<TcpManager>(new TcpManager("181.104.119.82", 4000));
+//builder.Services.AddSingleton<TcpManager>(new TcpManager("127.0.0.1", 22222));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +19,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 });
 
 var app = builder.Build();
+
+// Resolver el servicio para inicializarlo al inicio
+var tcpManager = app.Services.GetRequiredService<TcpManager>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- LaboratoryService_Api/Utilities/ConvertHL7.cs
+++ LaboratoryService_Api/Utilities/ConvertHL7.cs
@@ -15,10 +15,10 @@ namespace LaboratoryService_Api.Utilities
             try
             {
                 // Crear el mensaje HL7
-                var ormMessage = new ORM_O01();
+                var omlMessage = new OML_O33();
 
                 // MSH Segment
-                var msh = ormMessage.MSH;
+                var msh = omlMessage.MSH;
                 msh.FieldSeparator.Value = "|";
                 msh.EncodingCharacters.Value = "^~\\&";
                 msh.SendingApplication.NamespaceID.Value = "LaboratoryService";
@@ -33,7 +33,7 @@ namespace LaboratoryService_Api.Utilities
                 msh.VersionID.VersionID.Value = "2.5.1";
 
                 // PID Segment
-                var pid = ormMessage.PATIENT.PID;
+                var pid = omlMessage.PATIENT.PID;
                 pid.SetIDPID.Value = "1";
                 pid.PatientID.IDNumber.Value = registro.PacienteID.ToString();
                 var patientName = pid.GetPatientName(0);
@@ -42,11 +42,31 @@ namespace LaboratoryService_Api.Utilities
                 pid.DateTimeOfBirth.Time.Value = registro.Pacientes.FechadeNacimiento?.ToString("yyyyMMdd");
                 pid.AdministrativeSex.Value = registro.Pacientes.IdSexo == 1 ? "M" : "F"; // Asumiendo 1 para masculino y 2 para femenino
 
+                // PV1 Segment (Información de visita del paciente)
+                var pv1 = omlMessage.PATIENT.PATIENT_VISIT.PV1;
+                //pv1.PatientClass.Value = "O"; // Ambulatorio
+                pv1.PatientClass.Value = registro.InternacionID == 0 ? "0" : "1"; // Ambulatorio
+                pv1.AssignedPatientLocation.PointOfCare.Value = "AMB";
+                pv1.AdmissionType.Value = "E";
+                var attendingDoctor = pv1.GetAttendingDoctor(0);
+                attendingDoctor.IDNumber.Value = "123456";
+                //attendingDoctor.FamilyName.Surname.Value = "Doe";
+                attendingDoctor.FamilyName.Surname.Value = registro.Prestadores.Nombre.Trim();
+                //attendingDoctor.GivenName.Value = "John";
+                pv1.HospitalService.Value = "CAR";
+                pv1.AdmitDateTime.Time.Value = DateTime.Now.ToString("yyyyMMddHHmmss");
+
                 // ORC Segment (Grupos)
-                var orc = ormMessage.GetORDER(0).ORC;
+                var orc = omlMessage.GetSPECIMEN(0).GetORDER(0).ORC;
                 orc.OrderControl.Value = "NW"; // Nuevo pedido
                 orc.PlacerOrderNumber.EntityIdentifier.Value = registro.LaboratorioRegistroID.ToString();
                 orc.FillerOrderNumber.EntityIdentifier.Value = registro.CodigoBarra;
+
+                // NTE Segment (Notas y comentarios)
+                var nte = omlMessage.GetNTE(0);
+                nte.SetIDNTE.Value = "1";
+                nte.SourceOfComment.Value = "L"; // Laboratorio
+                nte.GetComment(0).Value = "Nota de prueba para el mensaje HL7";
 
                 // Agregar grupos al ORC
                 foreach (var detalle in registro.LabRegistroDetalle)
@@ -59,27 +79,36 @@ namespace LaboratoryService_Api.Utilities
                     }
                 }
 
+                // Iterar sobre cada muestra (specimen)
+                var specimen = omlMessage.GetSPECIMEN(omlMessage.SPECIMENRepetitionsUsed);
 
+                // Agregar el segmento SPM en SPECIMEN (y no en ORDER)
+                var spm = specimen.SPM;
+                spm.SetIDSPM.Value = "1";
+                spm.SpecimenID.PlacerAssignedIdentifier.EntityIdentifier.Value = registro.CodigoBarra;
+                spm.SpecimenType.Identifier.Value = "BLD"; // Sangre (Ejemplo)
+                spm.SpecimenType.Text.Value = "Sangre";
+                spm.SpecimenCollectionDateTime.RangeStartDateTime.Time.Value = DateTime.Now.ToString("yyyyMMddHHmmss");
 
                 // Iterar sobre cada práctica y agregar un nuevo ORDER con su propio OBR
                 foreach (var detalle in registro.LabRegistroDetalle)
                 {
                     // Agregar un nuevo ORDER para cada práctica
-                    var order = ormMessage.AddORDER();
-                    var obr = order.ORDER_DETAIL.OBR; // Obtener el OBR dentro del ORDER_DETAIL
+                    var order = omlMessage.GetSPECIMEN(0).AddORDER();
+                    var obr = order.OBSERVATION_REQUEST.OBR; // Obtener el OBR dentro del OBSERVATION_REQUEST
 
-                    obr.SetIDOBR.Value = (ormMessage.ORDERRepetitionsUsed).ToString(); // ID único por práctica
+                    obr.SetIDOBR.Value = (omlMessage.SPECIMENRepetitionsUsed).ToString(); // ID único por práctica
                     obr.PlacerOrderNumber.EntityIdentifier.Value = registro.LaboratorioRegistroID.ToString();
                     obr.FillerOrderNumber.EntityIdentifier.Value = registro.CodigoBarra;
                     obr.UniversalServiceIdentifier.Identifier.Value = detalle.LaboratorioPracticasID.ToString();
                     obr.UniversalServiceIdentifier.Text.Value = detalle.LaboratorioPracticas.Nombre;
                     obr.RequestedDateTime.Time.Value = registro.FechaCrea?.ToString("yyyyMMddHHmmss");
-                }
 
+                }
 
                 // Convertir el mensaje a string HL7
                 var parser = new PipeParser();
-                string hl7Message = parser.Encode(ormMessage);
+                string hl7Message = parser.Encode(omlMessage);
 
                 // Asegurar que los segmentos estén separados por \r y el mensaje termine con \n
                 hl7Message = hl7Message.Replace("\r\n", "\r").Replace("\n", "\r"); // Normalizar saltos de línea
@@ -90,7 +119,6 @@ namespace LaboratoryService_Api.Utilities
                 hl7Message += "\n"; // Añadir \n al final del mensaje
 
                 string mllpMessage = $"\x0B{hl7Message}\x1C\r"; // Encapsulado con MLLP
-
 
                 return mllpMessage;
             }
